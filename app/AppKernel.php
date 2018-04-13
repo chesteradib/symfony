50873bcaaa5ad85e5d831d3e0a1dccdaed72f31c
Adib Aroui <adib.aroui@hotmail.fr>
2018-04-13 20:20:42 +0100

    Trying to make doctrine fixtures work in prod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,7 +35,9 @@ class AppKernel extends Kernel
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
-        }s
+        }
+
+        return $bundles;
     }
 
     public function getRootDir()
