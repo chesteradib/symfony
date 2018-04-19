8e1dbde55ac495b403e514c4870cc607ba2104d4
Adib Aroui <adib.aroui@hotmail.fr>
2018-04-19 17:06:41 +0100

    git ignore var/logs

--- .provision/bootstrap-devenv.sh
+++ .provision/bootstrap-devenv.sh
@@ -23,12 +23,7 @@ apt-get install -y git acl zip pkg-config build-essential jpegoptim
 
 git clone https://github.com/chesteradib/symfony /vagrant
 
-apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip php-pear php-curl php7.0-gd
-
-
-phpenmod gd
-
-
+apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip php-pear php-curl
 
 echo "------Mysql 5.7 Installation "
 
